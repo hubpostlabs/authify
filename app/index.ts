171a3382a52846c86b524b8b91b376ab2f2c7076
iamayushy <ayushhuns@gmail.com>
2025-08-17 20:50:34 +0530

    added logger

--- app/index.ts
+++ app/index.ts
@@ -2,9 +2,12 @@ import { Hono } from 'hono'
 import authRoutes from '@/api/v1/selfauth/routes'
 import sessionRoutes from './api/v1/session/routes'
 import oauthRoutes from './api/v1/oauth/routes'
-import { logger } from "hono/logger"
+import { cors } from 'hono/cors'
 
 const app = new Hono()
+app.use(cors({
+  origin: ['https://bubble.hubpost.xyz']
+}))
 // app.use(logger());
 app.route("/api/v1/account", authRoutes)
 app.route("/api/v1/user", sessionRoutes)
